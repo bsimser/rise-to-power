41988adc614752e3e12be122f0857f11dccb2082
Matt Handley <applmak@google.com>
2013-10-03 15:57:38 -0500

    Added a silly redirect from /login to /map.

--- client/js/rtp/controllers/login-page.js
+++ client/js/rtp/controllers/login-page.js
@@ -1,6 +1,9 @@
 define(function(require) {
   var rtp = require('rtp/ui-module');
-  rtp.controller('LoginPageController', function() {
+  rtp.controller('LoginPageController', function($scope, $location) {
     console.log('LoginPageController inited');
+    $scope.returnToMap = function() {
+      $location.path('/map');
+    };
   });
 });
