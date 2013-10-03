cca628fed88bf1483e469655c8bf1a021ad72aef
Matt Handley <applmak@google.com>
2013-10-03 12:16:39 -0500

    Adding routes to the ui code for /login and otherwise. Factoring out the rtp ui module definition to its own module (for easy use with amd).

--- client/js/rtp/ui.js
+++ client/js/rtp/ui.js
@@ -1,7 +1,18 @@
 define(function(require) {
   var $ = require('jquery');
+  var rtp = require('rtp/ui-module');
   
-  var rtp = angular.module('rtp', []);
+  rtp.config(function($routeProvider) {
+    $routeProvider.when('/login', {
+      templateUrl: 'login.html',
+      controller: 'LoginPageController'
+    });
+    // /logout has no view, it redirects back to the main page.
+    $routeProvider.otherwise({
+      templateUrl: 'map.html',
+      controller: 'MapPageController'
+    });
+  });
   rtp.controller('MainController', function() {
     console.log('MainController loaded');
   });
