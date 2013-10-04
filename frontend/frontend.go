172b2f5c9bf8abb77656dd929f607362aa004d34
Jeremy Wall (zaphar) <jeremy@marzhillstudios.com>
2013-10-04 13:30:29 -0500

    go fmt

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -15,28 +15,28 @@
 package main
 
 import (
-  "flag"
-  "log"
-  "net/http"
+	"flag"
+	"log"
+	"net/http"
 
-  "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 var (
-  addr      = flag.String("address", ":8080", "Address to bind to.")
-  staticDir = flag.String("static_dir", "client", "Root directory for static files.")
-  muxer     = mux.NewRouter()
+	addr      = flag.String("address", ":8080", "Address to bind to.")
+	staticDir = flag.String("static_dir", "client", "Root directory for static files.")
+	muxer     = mux.NewRouter()
 )
 
 func quitQuitQuitHandler(w http.ResponseWriter, r *http.Request) {
-  log.Fatalf("%v requested we quit.", r.RemoteAddr)
+	log.Fatalf("%v requested we quit.", r.RemoteAddr)
 }
 
 func main() {
-  flag.Parse()
-  muxer.HandleFunc("/quitquitquit", quitQuitQuitHandler)
-  muxer.Handle("/", http.FileServer(http.Dir(*staticDir)))
-  http.Handle("/", muxer)
-  log.Printf("Server now listening on %v", *addr)
-  log.Fatal(http.ListenAndServe(*addr, nil))
+	flag.Parse()
+	muxer.HandleFunc("/quitquitquit", quitQuitQuitHandler)
+	muxer.Handle("/", http.FileServer(http.Dir(*staticDir)))
+	http.Handle("/", muxer)
+	log.Printf("Server now listening on %v", *addr)
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
