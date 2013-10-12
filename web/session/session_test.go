e615ea1bd9bd6cc01ccf7f3045c4c528b6c3bc96
Jeremy Wall <jwall@google.com>
2013-10-11 19:29:43 -0500

    Add an in memory implementation of a session store.
    Summary:
    * Implement the gorilla sessions.Store interface for an
      in memory implementation so we can use this for testing.
    * Add unittesets asserting that it works.
    
    Reviewers: swsnider
    
    Reviewed By: swsnider
    
    Differential Revision: http://d.sil.as/D36

---
+++ web/session/session_test.go
@@ -0,0 +1,53 @@
+// Copyright 2013 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package session
+
+import (
+	"testing"
+
+	"github.com/gorilla/sessions"
+)
+
+func TestSessionStorage(t *testing.T) {
+	// First assert that we satisfy the interface
+	var store sessions.Store
+	store = NewInMemoryStore()
+
+	// Now we want to test the actual concrete implementation.
+	memStore := store.(*inMemorySessionStore)
+	// No session exists case.
+	s, err := memStore.Get(nil, "foo session")
+	if s != nil || err != NoSuchSession {
+		t.Errorf("Expected nil session with error. Got %v with %v", s, err)
+	}
+
+	// Create a new session
+	s, err = memStore.New(nil, "foo session")
+	if s == nil || err != nil {
+		t.Errorf("Expected new session with no error. Got %v with %v", s, err)
+	}
+
+	// Save the session
+	if err = memStore.Save(nil, nil, s); err != nil {
+		t.Errorf("Unexpected error saving session. %v", err)
+	}
+
+	// Retrieve the session.
+	s, err = memStore.Get(nil, "foo session")
+	if s == nil || err != nil {
+		t.Logf("sessions: %v", memStore.store)
+		t.Errorf("Expected valid session with error. Got %v with %v", s, err)
+	}
+}
